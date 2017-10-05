6360c450b68fb3ebe890ab09deda40fcd3560db9
halfalpine <rocksandwoks@gmail.com>
2017-10-05 19:04:48 -0400

    Add route to update user email

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,6 @@
 import User from "../models/user";
 
-export function newUser(req, res, next) {
+export function newUser(req, res) {
   let newUser = new User({
     username: req.body.username,
     email: req.body.email
@@ -23,5 +23,16 @@ export function getUsers(req, res) {
   User.find({}, function(err, users) {
     if (err) console.log(err);
     res.json(users);
+  });
+}
+
+export function updateUser(req, res) {
+  User.findOne({ username: req.params.username }, function(err, user) {
+    if (err) throw err;
+    user.email = req.body.email;
+    user.save(function(err) {
+      if (err) throw err;
+      res.json(user);
+    });
   });
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,7 @@ import * as UserController from "../controllers/user";
 router.post("/new", UserController.newUser);
 
 router.get("/:username", UserController.getUser);
+router.put("/:username", UserController.updateUser);
 
 // OK
 router.get("/", UserController.getUsers);
